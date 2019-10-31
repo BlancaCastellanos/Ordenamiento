875d39559d3721b4e2d6f3d5a34fb5d05d16af4f
Blanca Castellanos <qtwre@outlook.com>
2019-10-31 12:57:00 -0700

    Cambios

--- Ordenamiento/MainWindow.xaml.cs
+++ Ordenamiento/MainWindow.xaml.cs
@@ -12,46 +12,48 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Collections.ObjectModel
+using System.Collections.ObjectModel;
 
 namespace Ordenamiento
 {
-    /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        ObservableCollection<int> miLista = new ObservableCollection<int>();
-        public MainWindow()
-        {
-            InitializeComponent();
-            miLista.Add(58);
-            miLista.Add(35);
-            miLista.Add(20);
-            miLista.Add(16);
-            miLista.Add(14);
-            miLista.Add(12);
-            miLista.Add(60);
-            miLista.Add(4);
+	/// <summary>
+	/// Lógica de interacción para MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		ObservableCollection<int> miLista = new ObservableCollection<int>();
+		public MainWindow()
+		{
+			InitializeComponent();
 
-            lstNumeros.ItemsSource = miLista;
-        }
+			miLista.Add(1);
+			miLista.Add(3);
+			miLista.Add(2);
+			miLista.Add(4);
+			miLista.Add(5);
+			miLista.Add(9);
+			miLista.Add(8);
+			miLista.Add(7);
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
+			lstNumeros.ItemsSource = miLista;
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
 			/*var temp = miLista[0];
+              miLista[0] = miLista[3];
+              miLista[3] = temp; */
 
-            miLista[0] = miLista[3];
-            miLista[3] = temp;*/
-
+			//SHELL
 			int gap, temp, i, j;
 			gap = miLista.Count / 2;
 
-			while (gap>0)
+			while (gap > 0)
 			{
-				for (i=0; i<miLista.Count; i++)
+
+				for (i = 0; i < miLista.Count; i++)
 				{
-					if(miLista[i] > miLista[gap + i])
+					if (gap + i < miLista.Count && miLista[i] > miLista[gap + i])
 					{
 						temp = miLista[i];
 						miLista[i] = miLista[gap + i];
@@ -61,6 +63,25 @@ namespace Ordenamiento
 
 				gap--;
 			}
-        }
-    }
+
+		}
+
+		private void Button_Click2(object sender, RoutedEventArgs e)
+		{
+			bool intercambio = false;
+			do
+			{
+				for (int i = 0; i < miLista.Count - 1; i++)
+				{
+					if (miLista[i] > miLista[i + 1])
+					{
+						int temp = miLista[i];
+						miLista[i] = miLista[i + 1];
+						miLista[i + 1] = temp;
+						intercambio = true;
+					}
+				}
+			} while (intercambio);
+		}
+	}
 }
